3772b835bb34f697dbebd469e33547560d9578e4
Atte Viitanen <atte.viitanen@aalto.fi>
2019-06-19 22:07:17 +0300

    Fix npm deploy script, make sure webhooks responses get sent before reboot

--- deploy/deployServer.js
+++ deploy/deployServer.js
@@ -17,6 +17,14 @@ if (!token) {
   process.exit();
 }
 
+const reset = () => {
+  shell.exec('pm2 stop bot')
+  shell.exec('git checkout master')
+  shell.exec('git reset --hard HEAD');
+  shell.exec('git pull origin master')
+  shell.exec('pm2 start index.js --name bot')
+}
+
 // Setup listening
 app.post('/', (req, res) => {
 
@@ -27,29 +35,20 @@ app.post('/', (req, res) => {
   }
 
   // Pull and reboot on successful merge requests
-  if (req &&
-    req.body &&
-    req.body.object_attributes &&
-    req.body.object_attributes.action &&
-    req.body.object_attributes.action === "merge") {
+  if (req.body.object_attributes.action === "merge") {
       console.log("GIT HOOK: New merge detected, rebooting")
-      shell.exec('pm2 stop bot')
-      shell.exec('git checkout master')
-      shell.exec('git reset --hard HEAD');
-      shell.exec('git pull origin master')
-      shell.exec('pm2 start index.js --name bot')
+      res.status(200).send('OK, rebooting bot!')
+      await reset()
   }
 
+  // The same for Gitlab merge event tests
   if (req.body.object_attributes.description === "Test merge") {
       console.log("GIT HOOK: New test merge detected, rebooting")
-      shell.exec('pm2 stop bot')
-      shell.exec('git checkout master')
-      shell.exec('git reset --hard HEAD');
-      shell.exec('git pull origin master')
-      shell.exec('pm2 start index.js --name bot')
+      res.status(200).send('OK, rebooting bot!')
+      await reset()
   }
   
-  res.sendStatus(200);
+  res.sendStatus(500);
 });
 
 app.listen(2277, () => console.log('Webhook is listening'));
